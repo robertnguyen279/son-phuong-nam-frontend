9437729f832a9feafe2759d6811e726cb34b74b3
Robert Nguyen <robert.nguyen279@gmail.com>
2022-05-11 18:41:54 +0700

    Add header

--- src/services/axios.service.ts
+++ src/services/axios.service.ts
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import jwt_decode from 'jwt-decode';
 
 // dev
 // const baseURL = 'https://tcbg3jx755.execute-api.ap-southeast-1.amazonaws.com';
@@ -10,41 +9,5 @@ const baseURL = 'https://bdxuiwz26k.execute-api.ap-southeast-1.amazonaws.com';
 const axiosInstance = axios.create({
   baseURL,
 });
-
-axiosInstance.interceptors.request.use(
-  async (req): Promise<typeof req> => {
-    const authTokens = localStorage.getItem('SonPhuongNamAuthTokens')
-      ? JSON.parse(localStorage.getItem('SonPhuongNamAuthTokens'))
-      : null;
-
-    if (!authTokens) {
-      return req;
-    }
-
-    req.headers.Authorization = `Bearer ${authTokens?.accessToken}`;
-
-    const user: { userId: string; iat: number; exp: number } = jwt_decode(authTokens.accessToken);
-
-    if (Date.now() <= user.exp * 1000) {
-      return req;
-    }
-
-    try {
-      const response = await axios.post(`${baseURL}/user/token/`, {
-        token: authTokens.refreshToken,
-      });
-
-      localStorage.setItem(
-        'SonPhuongNamAuthTokens',
-        JSON.stringify({ accessToken: response.data.accessToken, refreshToken: response.data.refreshToken }),
-      );
-      req.headers.Authorization = `Bearer ${response.data.accessToken}`;
-    } catch (error) {
-      console.log({ ...error });
-    }
-
-    return req;
-  },
-);
 
 export default axiosInstance;
